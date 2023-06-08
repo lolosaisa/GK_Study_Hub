03513741fd5105d77c3345991fa990f5aeaedae7
lolo saisa <lwamaitha3924@gmail.com>
2023-06-08 02:10:48 -0700

    added Assets folder
    modified the landing page

--- lib/LandingPage/Landingpage.dart
+++ lib/LandingPage/Landingpage.dart
@@ -21,122 +21,113 @@ class DesktopNavbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            _buildHeroSection(),
-            _buildFeaturesSection(),
-            _buildGetStartedSection(),
-            _buildFooterSection(),
-           
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildHeroSection() {
-    return Container(
-      height: 600,
-      color: Colors.lightGreen,
-      child: const Center(
-        child: Text(
-          'Welcome to GK Study Hub',
-          style: TextStyle(
-              color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildFeaturesSection() {
-    return Container(
-      height: 400,
-      color: Colors.grey[200],
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children:  [
-          Text(
-            'Our Features',
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              _buildFeatureItem(Icons.video_library, 'Video Lessons'),
-              _buildFeatureItem(
-                  Icons.lightbulb_outline, 'Interactive Exercises'),
-              _buildFeatureItem(Icons.school, 'Courses'),
-            ],
-          ),
-        ],
-      ),
-      
-    );
-  }
-
-  Widget _buildFeatureItem(IconData icon, String Text) {
-    return Column(
+        body: PageView(
+      scrollDirection: Axis.vertical,
       children: [
-        Icon(
-          icon,
-          size: 60,
-          color: Colors.lightGreenAccent,
-        ),
-        SizedBox(height: 10),
-        Text(TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-        ))
+        section1(),
+        section2(),
+        section3(),
+        section4(),
       ],
-    );
+    ));
   }
-  Widget _buildGetStartedSection(){
-    return Container(
-      height: 200,
-      color: Colors.lightGreen,
-      child: Center(
-        child: RaisedButton(
-          onpressed(){},
-          color: Colors.white,
-          textColor: Colors.green[100],
-          Padding: EdgeInsets.symmetric( vertical: 15, horizontal: 30),
-          shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(30),
-          ),
-          child: const Text('Get Started',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-          ),
+}
 
+class section1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          'Assets/images/teacher.jpg',
+          height: 200,
         ),
-      ),
-    );
+        const SizedBox(
+          width: 20,
+        ),
+        const Text(
+          'for',
+          style: TextStyle(fontSize: 24, color: Colors.white),
+        )
+      ],
+    ));
   }
-  
-  RaisedButton(Null Function() param0, {required Color color, Color? textColor, required EdgeInsets Padding, required RoundedRectangleBorder shape, required Text child}) {}
+}
 
-  Widget _buildFooterSection(){
+class section2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      height: 100,
-      child: Center(
-        child: Text('@ 2023 GK Study Hub. All Rights Reserves.',
-        style: TextStyle(
-          fontSize: 16,
-        ),),
-      ),
-    );
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          'Assets/images/teacher.jpg',
+          height: 200,
+        ),
+        const SizedBox(
+          width: 20,
+        ),
+        const Text(
+          'for ',
+          style: TextStyle(fontSize: 24, color: Colors.white),
+        )
+      ],
+    ));
   }
+}
 
-  
+class section3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          'Assets/images/teacher.jpg',
+          height: 200,
+        ),
+        const SizedBox(
+          width: 20,
+        ),
+        const Text(
+          'for er',
+          style: TextStyle(fontSize: 24, color: Colors.white),
+        )
+      ],
+    ));
+  }
+}
+
+class section4 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          'Assets/images/teacher.jpg',
+          height: 200,
+        ),
+        const SizedBox(
+          width: 20,
+        ),
+        const Text(
+          'For Every ClassRoom',
+          style: TextStyle(fontSize: 24, color: Colors.white),
+        )
+      ],
+    ));
+  }
 }
 
 class MobileNavbar extends StatelessWidget {
+  const MobileNavbar({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container();
